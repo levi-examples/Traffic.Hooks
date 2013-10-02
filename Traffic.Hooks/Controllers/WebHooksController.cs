83705a8144838be447f92e9afc02ce29ab4a8cdd
Levi Wilson <levi@leviwilson.com>
2013-10-01 22:44:21 -0400

    initial checkin

---
+++ Traffic.Hooks/Controllers/WebHooksController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Traffic.Hooks.Controllers
+{
+    public class WebHooksController : ApiController
+    {
+        public string[] Get()
+        {
+            return new [] { "Hello", "World" };
+        }
+    }
+}
