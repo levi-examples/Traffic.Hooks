8734d45bfda13194facccb2e81e45e5a1a003316
Levi Wilson <levi@leviwilson.com>
2013-10-01 23:20:27 -0400

    do something async

--- Traffic.Hooks/Controllers/WebHooksController.cs
+++ Traffic.Hooks/Controllers/WebHooksController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Web.Http;
 
 namespace Traffic.Hooks.Controllers
 {
@@ -6,7 +9,25 @@ namespace Traffic.Hooks.Controllers
     {
         public string[] Get()
         {
-            return new [] { "Hello", "World" };
+            BackgroundTaskManager.Run(() => DoSomethingLater());
+            return new[] { "Hello", "World" };
+        }
+
+        private static void DoSomethingLater()
+        {
+            var whenItStarted = DateTime.Now;
+            Thread.Sleep(5000);
+            File.AppendAllText(TemporaryFile, Message(whenItStarted, DateTime.Now));
+        }
+
+        protected static string TemporaryFile
+        {
+            get { return Path.GetTempPath() + "AsyncWebApiTest.txt"; }
+        }
+
+        private static string Message(DateTime startTime, DateTime endTime)
+        {
+            return string.Format("Started at {0} and ended at {1}{2}", startTime, endTime, Environment.NewLine);
         }
     }
 }
